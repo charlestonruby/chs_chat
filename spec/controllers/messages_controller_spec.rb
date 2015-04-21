d098d0b10fd53dd966d33c8a1bdb1facd00ce870
William Holt <wtholt27@gmail.com>
2015-04-20 22:21:41 -0400

    testing messages controller

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
 
+  describe 'GET #index' do 
+    it 'populates an array of messages' do 
+      channel = FactoryGirl.create(:channel)
+      message = FactoryGirl.create(:message)
+      get :index, format: :json, channel_id: channel
+      assigns(:messages).should eq([message])
+    end
+    it 'renders the :index view' do 
+      channel = FactoryGirl.create(:channel)
+      get :index, format: :json, channel_id: channel
+    end
+  end
+
+  describe 'GET #show' do 
+    it 'assigns the requested message to @message'
+    it 'renders the :show template'
+  end
+
+  describe 'GET #new' do 
+    it 'assigns a new Message to @message'
+    it 'renders the :new template'
+  end
+
 end
